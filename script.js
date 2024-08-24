03fb5b59808a1066564292fc47d33c785e73bc06
zyrox-e <d.hacquard73@gmail.com>
2024-08-24 16:10:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    var envelope = $('#envelope');
+    var letter = $('#letter');
+    var btn_open = $("#open");
+    var btn_reset = $('#reset');
+    
+    envelope.click(function() {
+        open();
+    });
+    btn_open.click(function() {
+        open();
+    });
+    btn_reset.click(function() {
+        close();
+    });
+    letter.click(function() {
+        expandLetter();
+    })
+    
+    function open() {
+        envelope.addClass('open')
+            .removeClass('close');
+    }
+    function close() {
+        envelope.addClass('close')
+            .removeClass('open');
+    }
+    function expandLetter(){
+        letter.addClass('read')
+    }
+})
